0a88f6d39c5546cb32e01e790f40a0bce6ee95f0
ProWhat <kir.nik.nikita@gmail.com>
2024-03-07 00:41:50 +0400

    creating new functionality with funct include

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -1,6 +1,26 @@
-from django.urls import path
+from django.urls import path, re_path, include
 from hello import views
 
+
+
+products_patterns = [
+    path("", views.products),
+    path("new", views.new),
+    path("top", views.top),
+]
+
+product_patterns = [
+    path("", views.product),
+    path("comments", views.comments),
+    path("questions", views.questions),
+]
+
+
+# Очередность маршрутов важна!
 urlpatterns = [
-    path('', views.index, name='home'),
+    re_path(r"^about", views.about, kwargs={"name": "Nikita", "age": 23}),
+    re_path(r"^contact", views.contact),
+    path("", views.index),
+    path("products/<int:p_id>/", include(product_patterns)),
+    path("products/", include(products_patterns)),
 ]
--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,48 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("<h1>Главная</h1>")
+
+
+def about(request, name, age):
+    return HttpResponse(f"""
+            <h2>О пользователе</h2>
+            <p>Имя: {name}</p>
+            <p>Возраст: {age}</p>
+    """)
+
+
+def contact(request):
+    host = request.META["HTTP_HOST"]                # получаем адрес сервера
+    user_agent = request.META["HTTP_USER_AGENT"]    # получаем данные браузера
+    path = request.path                             # получаем запрошенный путь
+
+    return HttpResponse(f"""
+            <p>Host: {host}</p>
+            <p>Path: {path}</p>
+            <p>User-agent: {user_agent}</p>
+        """)
+
+
+def products(request):
+    return HttpResponse("Список товаров")
+
+
+def new(request):
+    return HttpResponse("Новые товары")
+
+
+def top(request):
+    return HttpResponse("Наиболее популярные товары")
+
+
+def product(request, p_id):
+    return HttpResponse(f"Товар {p_id}")
+
+
+def comments(request, p_id):
+    return HttpResponse(f"Комментарии о товаре {p_id}")
+
+
+def questions(request, p_id):
+    return HttpResponse(f"Вопросы о товаре {p_id}")
